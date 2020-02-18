c6bf628ed746859155a536faaf69a6a1d059c5e9
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2020-02-18 01:21:21 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -27,7 +27,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # Christmas AND the New Year's arrays
   winter_holidays = holiday_hash[:winter]
   winter_holidays.each_value do |supplies|
-    supplies << "Balloons"
+    supplies << "supply"
   end
 end
 
@@ -35,7 +35,7 @@ end
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
-
+  holiday_hash[:spring]
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)

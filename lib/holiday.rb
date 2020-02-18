0111f33795eed449dab0bd567d24a1117e18064e
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2020-02-18 04:43:52 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -70,5 +70,12 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  holidays_with_bbqs = []
+  holiday_hash.each do |season, holidays|
+    holidays.each do |holiday, supplies|
+      if supplies.include?("BBQ")
+        holidays_with_bbqs << holiday
+      end
+    end
+  end
 end
